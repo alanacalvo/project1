990c78d0eabbf3c10a020827b7828a455074078f
alanacalvo <alana.keeton@gmail.com>
2022-03-15 20:22:12 -0400

    fml

--- js-toolkit/extra.js
+++ js-toolkit/extra.js
@@ -39,13 +39,13 @@ const handleRight = (arr, i, obj, node) => {
 // LEFT ARROW FUNCTION
 const handleLeft = (arr, i, obj, node) => {
     const visibleNums = document.querySelectorAll(`.${node}`);
-    const current = arr[i]
-    const next = arr[i-1]
-    const previous = arr[i+1]
-    const sum = current + next;
+    // const current = arr[i]
+    // const next = arr[i-1]
+    // const previous = arr[i+1]
+    const sum = arr[i-1] + arr[i];
     // MERGER
-  if (current !== 0 && current === next && obj[i] === false) {
-        arr[i-1] += current;
+  if (arr[i] !== 0 && arr[i] === arr[i-1] && obj[i] === false) {
+        arr[i-1] += arr[i];
         arr[i] = 0;
         visibleNums[i-1].innerText = sum;
         visibleNums[i].innerText = 0;
@@ -53,13 +53,13 @@ const handleLeft = (arr, i, obj, node) => {
         obj[i-1] = true;
     } 
     // MOVE FORWARD + REPLACE WITH ZERO 
-    else if (current !== 0 && next === 0) {
-        arr[i-1] += current;
+    else if (arr[i] !== 0 && arr[i-1] === 0) {
+        arr[i-1] += arr[i];
         arr[i] = 0;
-        visibleNums[i-1].innerText = sum;
+        visibleNums[i-1].innerText = arr[i-1];
         visibleNums[i].innerText = 0;
     } 
-    else if (previous === 0 && i < 2) {
+    else if (arr[i+1] === 0 && i < 2) {
         const slice = arr.slice(0, i+1)
         const slice2 = arr.slice(i+2)
         const newArray = slice.concat(slice2)
@@ -74,8 +74,174 @@ const handleLeft = (arr, i, obj, node) => {
         visibleNums[3].innerText = newArray[3];
     }
 }
-// UP ARROW FUNCTION
-
-
-
 // DOWN ARROW FUNCTION
+const handleDown = (arr1, arr2, arr3, arr4, i, obj1, obj2, obj3, obj4, node1, node2, node3, node4) => {
+    const visibleNums1 = document.querySelectorAll(`.${node1}`);
+    const visibleNums2 = document.querySelectorAll(`.${node2}`);
+    const visibleNums3 = document.querySelectorAll(`.${node3}`);
+    const visibleNums4 = document.querySelectorAll(`.${node4}`);
+    if (arr4[i] !== 0 && arr4[i] === arr3[i] && obj3[i] === false && obj4[i] === false) {
+        arr4[i] += arr3[i];
+        arr3[i] = 0;
+        obj3[i] = true;
+        obj4[i] = true;
+        const sum = arr4[i] + arr3[i];
+        visibleNums4[i].innerText = sum;
+        // console.log(visibleNums4[i].innerText)
+        visibleNums3[i].innerText = 0;
+        // console.log('arr4',arr4, 'arr3', arr3)
+    }
+    if (arr3[i] !== 0 && arr3[i] === arr2[i] && obj2[i] === false && obj3[i] === false) {
+        arr3[i] += arr2[i];
+        arr2[i] = 0;
+        obj3[i] = true;
+        obj2[i] = true;
+        const sum = arr3[i] + arr2[i];
+        visibleNums3[i].innerText = sum;
+        // console.log(visibleNums3[i].innerText)
+        visibleNums2[i].innerText = 0;
+        // console.log('arr3',arr3, 'arr2', arr2)
+    }
+    if (arr2[i] !== 0 && arr2[i] === arr1[i] && obj1[i] === false && obj2[i] === false) {
+        arr2[i] += arr1[i];
+        arr1[i] = 0;
+        obj2[i] = true;
+        obj1[i] = true;
+        const sum = arr2[i] + arr1[i];
+        visibleNums2[i].innerText = sum;
+        visibleNums1[i].innerText = 0;
+        // console.log('arr2',arr2, 'arr1', arr1)
+    }
+    if (arr4[i] === 0) {
+        if (arr3[i] !== 0) {
+            arr4[i] = arr3[i];
+            arr3[i] = 0;
+            visibleNums4[i].innerText = arr4[i];
+            visibleNums3[i].innerText = 0;
+        } else if (arr2[i] !== 0 && arr3[i] === 0) {
+            arr4[i] = arr2[i];
+            arr2[i] = 0;
+            visibleNums4[i].innerText = arr4[i];
+            visibleNums2[i].innerText = 0;
+        } else if (arr1[i] !== 0 && arr2[i] === 0 && arr3[i] === 0) {
+            arr4[i] = arr1[i];
+            arr1[i] = 0;
+            visibleNums4[i].innerText = arr4[i];
+            visibleNums1[i].innerText = 0;
+        }
+    } else if (arr3[i] === 0) {
+        if (arr2[i] !== 0) {
+            arr3[i] = arr2[i];
+            arr2[i] = 0;
+            visibleNums3[i].innerText = arr3[i];
+            visibleNums2[i].innerText = 0;
+        } else {
+            if (arr1[i] !== 0) {
+                arr3[i] = arr1[i];
+                arr1[i] = 0;
+                visibleNums3[i].innerText = arr3[i];
+                visibleNums1[i].innerText = 0;
+            }
+        }
+    } else if (arr2[i] === 0) {
+        if (arr1[i] !== 0) {
+            arr2[i] = arr1[i];
+            arr1[i] = 0;
+            visibleNums2[i].innerText = arr2[i];
+            visibleNums1[i].innerText = 0;
+        }
+    }
+    // console.log(visibleNums1[i].innerText)
+    // console.log(visibleNums2[i].innerText)
+    // console.log(visibleNums3[i].innerText)
+    // console.log(visibleNums4[i].innerText)
+    // console.log(arr1, 'arr1')
+    // console.log(arr2, 'arr2')
+    // console.log(arr3, 'arr3')
+    // console.log(arr4, 'arr4')
+}
+// HANDLE UP
+const handleUp = (arr1, arr2, arr3, arr4, i, obj1, obj2, obj3, obj4, node1, node2, node3, node4) => {
+    const visibleNums1 = document.querySelectorAll(`.${node1}`);
+    const visibleNums2 = document.querySelectorAll(`.${node2}`);
+    const visibleNums3 = document.querySelectorAll(`.${node3}`);
+    const visibleNums4 = document.querySelectorAll(`.${node4}`);
+    if (arr1[i] !== 0 && arr1[i] === arr2[i] && obj2[i] === false && obj1[i] === false) {
+        const sum = arr1[i] + arr2[i];
+        arr1[i] += arr2[i];
+        arr2[i] = 0;
+        obj2[i] = true;
+        obj1[i] = true;
+        visibleNums1[i].innerText = sum;
+        visibleNums2[i].innerText = 0;
+        console.log('arr1',arr1, 'arr2', arr2)
+    }
+    if (arr2[i] !== 0 && arr2[i] === arr3[i] && obj2[i] === false && obj3[i] === false) {
+        const sum = arr2[i] + arr3[i];
+        arr2[i] += arr3[i];
+        arr3[i] = 0;
+        obj3[i] = true;
+        obj2[i] = true;
+        visibleNums2[i].innerText = sum;
+        visibleNums3[i].innerText = 0;
+        console.log('arr2',arr2, 'arr3', arr3)
+    }
+    if (arr3[i] !== 0 && arr3[i] === arr4[i] && obj3[i] === false && obj4[i] === false) {
+        arr3[i] += arr4[i];
+        arr4[i] = 0;
+        obj3[i] = true;
+        obj4[i] = true;
+        const sum = arr3[i] + arr4[i];
+        visibleNums3[i].innerText = sum;
+        visibleNums4[i].innerText = 0;
+        // let score = sum += sum ?
+    }
+    if (arr1[i] === 0 && arr2[i] !== 0) {
+        arr1[i] = arr2[i];
+        arr2[i] = 0;
+        visibleNums1[i].innerText = arr1[i];
+        visibleNums2[i].innerText = 0;
+    }
+    if (arr3[i] !== 0 && arr2[i] === 0) { 
+        arr2[i] = arr3[i];
+        arr3[i] = 0;
+        visibleNums2[i].innerText = arr2[i];
+        visibleNums3[i].innerText = 0; 
+    }
+    if (arr4[i] !== 0 && arr1[i] === 0 && arr2[i] === 0 && arr3[i] === 0) {
+        arr1[i] = arr4[i];
+        arr4[i] = 0;
+        visibleNums1[i].innerText = arr1[i];
+        visibleNums4[i].innerText = 0;
+    }
+    if (arr1[i] !== 0 && arr2[i] === 0 && arr3[i] === 0 && arr4[i] !== 0) {
+        arr2[i] = arr4[i];
+        arr4[i] = 0;
+        visibleNums2[i].innerText = arr2[i];
+        visibleNums4[i].innerText = 0;
+    }   
+    if (arr2[i] === 0 && arr3[i] !== 0) {
+        arr2[i] = arr3[i];
+        arr3[i] = 0;
+        visibleNums2[i].innerText = arr2[i];
+        visibleNums3[i].innerText = 0;
+    }
+    if (arr3[i] === 0 && arr4[i] !== 0) {
+        arr3[i] = arr4[i];
+        arr4[i] = 0;
+        visibleNums3[i].innerText = arr3[i];
+        visibleNums4[i].innerText = 0;
+        }
+    if (arr2[i] === 0 && arr4[i] !== 0) {
+        arr2[i] = arr4[i];
+        arr4[i] = 0;
+        visibleNums2[i].innerText = arr2[i];
+        visibleNums4[i].innerText = 0;
+        }
+    if (arr1[i] === 0 && arr2[i] !== 0) {
+        arr1[i] = arr2[i]
+        arr2[i] = 0
+        visibleNums1[i].innerText = arr1[i];
+        visibleNums2[i].innerText = 0;
+    }
+}
--- js-toolkit/script.js
+++ js-toolkit/script.js
@@ -1,7 +1,7 @@
-let a1 = [0,0,0,0]
-let a2 = [0,0,0,0]
-let a3 = [0,0,0,0]
-let a4 = [0,0,0,0]
+let a1 = [2,0,0,16]
+let a2 = [2,8,2,0]
+let a3 = [16,2,2,0]
+let a4 = [4,16,0,4]
 
 const trackerObj1 = {
     0: false,
@@ -29,8 +29,8 @@ const trackerObj4 = {
 }
 
 const initializeGame = (arr1, arr2, arr3, arr4) => {
-    twoRandomTwosOnLoad(arr1, arr2, arr3, arr4)
-    twoRandomTwosOnLoad(arr1, arr2, arr3, arr4)
+    // twoRandomTwosOnLoad(arr1, arr2, arr3, arr4)
+    // twoRandomTwosOnLoad(arr1, arr2, arr3, arr4)
     //CREATE EACH DIV AND APPEND ARRAYS
     for (let i = 0; i < arr1.length; i++) {
         let newDiv1 = document.createElement('div');
@@ -58,28 +58,29 @@ const initializeGame = (arr1, arr2, arr3, arr4) => {
     }
 }
 
-const twoRandomTwosOnLoad = (arr1, arr2, arr3, arr4) => {
-    let randomArray = Math.floor(Math.random() * 4);
-    let randomIndex = Math.floor(Math.random() * 4);
-console.log('arr', randomArray, 'index', randomIndex)
-    if (arr0,arr1,arr2,arr3 !== 0) {
-        if (randomArray === 0) {
-            arr1[randomIndex] = 2;
-        } else if (randomArray === 1) {
-            arr2[randomIndex] = 2;
-        } else if (randomArray === 2) {
-            arr3[randomIndex] = 2;
-        } else if (randomArray === 3) {
-            arr4[randomIndex] = 2;
-        } 
-    }
-}
+// const twoRandomTwosOnLoad = (arr1, arr2, arr3, arr4) => {
+//     let randomArray = Math.floor(Math.random() * 4);
+//     let randomIndex = Math.floor(Math.random() * 4);
+// console.log('arr', randomArray, 'index', randomIndex)
+//     if (arr1,arr2,arr3,arr4 !== 0) {
+//         if (randomArray === 0) {
+//             arr1[randomIndex] = 2;
+//         } else if (randomArray === 1) {
+//             arr2[randomIndex] = 2;
+//         } else if (randomArray === 2) {
+//             arr3[randomIndex] = 2;
+//         } else if (randomArray === 3) {
+//             arr4[randomIndex] = 2;
+//         } 
+//     }
+// }
 
 // KEYDOWN FUNCTION THAT HOLDS ALL 4 ARROW FUNCTIONS
 document.onkeydown = function(e) {
     if (e.keyCode === 39) {
-        for (let i = 0; i < 4; i++){
-            handleRight(a1, i, trackerObj1, 'newDiv1')
+        // RIGHT KEY
+        for (let i = 0; i < 4; i++){ // state.forEach
+            handleRight(a1, i, trackerObj1, 'newDiv1') //rotateRight ()  *4?
             handleRight(a2, i, trackerObj2, 'newDiv2')    
             handleRight(a3, i, trackerObj3, 'newDiv3')
             handleRight(a4, i, trackerObj4, 'newDiv4')       
@@ -88,8 +89,10 @@ document.onkeydown = function(e) {
         resetObj(trackerObj2)
         resetObj(trackerObj3)
         resetObj(trackerObj4)
-         twoRandomTwosOnLoad(a1, a2, a3, a4)
+        
+        //  twoRandomTwosOnLoad(a1, a2, a3, a4)
     } else if (e.keyCode === 37) {
+        // LEFT KEY
         for (let i = 3; i >= 0; i--) {
             handleLeft(a1, i, trackerObj1, 'newDiv1')
             handleLeft(a2, i, trackerObj2, 'newDiv2')    
@@ -100,10 +103,28 @@ document.onkeydown = function(e) {
         resetObj(trackerObj2)
         resetObj(trackerObj3)
         resetObj(trackerObj4)
-        twoRandomTwosOnLoad(a1, a2, a3, a4)
-    } else if (e.keyCode === 38) {
+        // twoRandomTwosOnLoad(a1, a2, a3, a4)
+    } 
+    else if (e.keyCode === 38) {
+        // UP KEY
+        for (let i = 0; i < a1.length; i++) {
+            handleUp(a1,a2,a3,a4,i,trackerObj1,trackerObj2,trackerObj3,trackerObj4,'newDiv1','newDiv2','newDiv3','newDiv4')
+        }
+        resetObj(trackerObj1)
+        resetObj(trackerObj2)
+        resetObj(trackerObj3)
+        resetObj(trackerObj4)
         console.log('up key pressed')
-    } else if (e.keyCode === 40) {
+    } 
+    else if (e.keyCode === 40) {
+        // DOWN KEY
+        for (let i = 0; i < a1.length; i++) {
+            handleDown(a1,a2,a3,a4,i,trackerObj1,trackerObj2,trackerObj3,trackerObj4,'newDiv1','newDiv2','newDiv3','newDiv4')
+        }
+        resetObj(trackerObj1)
+        resetObj(trackerObj2)
+        resetObj(trackerObj3)
+        resetObj(trackerObj4)
         console.log('down key pressed')
     }
 }
@@ -114,3 +135,6 @@ const resetObj = (obj) => {
         obj[key] = false;
     })
 }
+
+
+
